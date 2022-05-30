84c9325633db161b0364ddf9f1cd916243a7b88c
Kareem Gamal <88196791+kareemgamal1@users.noreply.github.com>
2022-05-30 03:51:56 +0200

    Finish Project.

---
+++ Host/host.h
@@ -0,0 +1,30 @@
+#ifndef HOST_H
+#define HOST_H
+#include "../Place/place.h"
+#include <filesystem>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Host : public User
+{
+    unordered_map<int,Place> places; // vector
+public:
+    void signup();
+    void login();
+    void showChoices();
+    string validateEmail(string email);
+    bool checkForCredintials(string email, string password);
+    void fillHostInfo(string path);
+    void serializePlace(Place p);
+    void deSerializePlaces();
+    void addAdvertisement();
+    void displayAdvertisements();
+    void editAdvertisement();
+    void deleteAdvertisement();
+    void showSpecificPlace();
+    Host(string password, string firstName, string lastName, string email, string phone, string nationality, char gender, int age);
+    Host();
+};
+#endif
