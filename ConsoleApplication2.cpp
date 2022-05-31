8db2bc97aa680d6a5f1bc25295fb6e6cbf7b5dda
Kareem Gamal <88196791+kareemgamal1@users.noreply.github.com>
2022-05-31 04:15:19 +0200

    Add files via upload

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -4,33 +4,33 @@ int main()
 {
 	int choice;
 	while (true) {
-		cout << "-Choose (1) to sign up as a host.\n-Choose (2) sign up as traveller\n-Choose (3) to login as a host.\n-Choose (4) to login as a traveller. \n if you want to login as admin press 5 \n ";
+		cout << "-Choose (1) to sign up as a host.\n-Choose (2) sign up as traveller\n-Choose (3) to login as a host.\n-Choose (4) to login as a traveller. \n-Choose (0) if you want to exit the program.\nChoose (5) if you want to login as admin.\n\n";
 		cin >> choice;
 		if (choice == 1) {
 			Host h;
 			h.signup();
 			h.login();
-			break;
 		}
 		else if (choice == 2) {
 			Traveler T;
 			T.signup();
 			T.login();
-			break;
 		}
 		else if (choice == 3) {
 			Host h;
 			h.login();
-			break;
 		}
 		else if (choice == 4) {
 			Traveler T;
 			T.login();
-			break;
 		}
 		else if (choice == 5) {
 			Admin a;
-			a.mainmenu();
+			a.validateAdmin();
+		}
+		else if (choice == 0) {
+			cout << "Thank you for using our program!\n";
+			break;
 		}
 		else
 		{
